a1ccacdff376f541982a181ea53c588e8bdf66d5
ghernandez <ghernandez@hircasa.com>
2023-03-26 14:05:52 -0600

    commit inicial

---
+++ sitioindicadores/empresaprueba/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+# Create your views here.
+@login_required
+def home(request):
+    username = request.user.username
+    context = {'username': username}
+    return render(request, 'Home/home.html', context)
+
+def salir(request):
+    logout(request)
+    return redirect('/')
